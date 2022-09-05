fdb5c3a077019e4f9efa090fa44e23a12502d610
olegukr <oleg.poberezhets@gmail.com>
2022-09-05 23:03:50 +0200

    4.1-4.5 completed

--- exercises/04_data_structures/task_4_5.py
+++ exercises/04_data_structures/task_4_5.py
@@ -22,3 +22,10 @@
 
 command1 = "switchport trunk allowed vlan 1,2,3,5,8"
 command2 = "switchport trunk allowed vlan 1,3,8,9"
+
+result_1 = set(command1.split()[-1].split(","))
+# print (result_1)
+result_2 = set(command2.split()[-1].split(","))
+# print (result_2)
+result = sorted(result_1 & result_2)
+print (result)
