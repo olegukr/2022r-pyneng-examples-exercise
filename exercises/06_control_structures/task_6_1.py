a1ea0bb8b453d364fa2ce462c82fe862c94644bf
olegukr <oleg.poberezhets@gmail.com>
2024-09-10 21:11:17 +0200

    6.1 completed

--- exercises/06_control_structures/task_6_1.py
+++ exercises/06_control_structures/task_6_1.py
@@ -13,3 +13,7 @@ MAC-адреса используются в формате XXXX.XXXX.XXXX
 """
 
 mac = ["aabb:cc80:7000", "aabb:dd80:7340", "aabb:ee80:7000", "aabb:ff80:7000"]
+result0 = ' '.join(mac)
+result1 = result0.replace(":", ".")
+result = result1.split()
+print(result)
